ea6d8f6bf49991bfa881f7b212f905d823c00ecf
markus <markus331@gmail.com>
2016-11-13 14:40:43 +0100

    ignore GcsAdapterTest

--- src/test/java/org/wahlzeit/model/persistence/GcsAdapterTest.java
+++ src/test/java/org/wahlzeit/model/persistence/GcsAdapterTest.java
@@ -2,11 +2,13 @@ package org.wahlzeit.model.persistence;
 
 import com.google.appengine.tools.development.testing.LocalBlobstoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.junit.Ignore;
 
 /**
  * Test cases for the Google Cloud Storage adapter.
  * @review
  */
+@Ignore
 public class GcsAdapterTest extends AbstractAdapterTest {
 
 	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalBlobstoreServiceTestConfig());
